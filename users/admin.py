f62beb4d4a009bf6ed021faf97f877d02bf9466a
Davyd <davidkrivko@gmail.com>
2023-11-09 16:53:03 +0100

    Add models to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserModel, CategoryModel
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'gender', 'birth_date', 'category')
+    list_filter = ('category', 'gender', 'birth_date')
+    search_fields = ('first_name', 'last_name', 'email')
+
+
+admin.site.register(UserModel, UserModelAdmin)
+admin.site.register(CategoryModel)
